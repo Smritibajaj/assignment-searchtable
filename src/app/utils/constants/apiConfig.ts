bafa5078ea27e9e6d2715e55b636a058604b1d55
simmy bajaj <simmybajaj1603@gmail.com>
2022-12-20 10:54:56 +0530

    changes

--- src/app/utils/constants/apiConfig.ts
+++ src/app/utils/constants/apiConfig.ts
@@ -1,4 +1,4 @@
 /* 
     API URL FOR MAKE REQUEST TO SERVER
 */
-export const API_DOMAIN: string = process.env.REACT_APP_API_DOMAIN || 'https://assignment-searchtable-lzeoco4do-smritibajaj.vercel.app/';
+export const API_DOMAIN: string = process.env.REACT_APP_API_DOMAIN || 'https://assignment-searchtable-be.vercel.app/';
